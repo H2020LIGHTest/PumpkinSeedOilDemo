54fe488e560aad6f386365f3872ffe86a210589b
Stefan More <stefan.more@iaik.tugraz.at>
2019-12-12 00:12:18 +0100

    ec-demo-client: added container verifier testrun

---
+++ _work/client/src/test/java/eu/lightest/demo/VerifyContainerTest.java
@@ -0,0 +1,49 @@
+package eu.lightest.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class VerifyContainerTest {
+    
+    private static Object pathPolicy = "../policies/policy_pof_withTranslation.tpl";
+    
+    @Parameterized.Parameter(0)
+    public String container;
+    
+    @Parameterized.Parameter(1)
+    public String policy;
+    
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> data() {
+        List data = new ArrayList();
+        
+        final FilenameFilter filter = (File dir, String name) -> {
+            return name.endsWith(".asic") || name.endsWith(".asice") || name.endsWith(".asics");
+        };
+        
+        File container = new File(CreateContainer.orders);
+        for(File file : container.listFiles(filter)) {
+            data.add(new Object[]{file.getPath(), pathPolicy});
+        }
+        
+        return data;
+    }
+    
+    @Test
+    public void verifyContainer() {
+        boolean status = VerifyContainer.verify(container, policy);
+        
+        assertTrue(status);
+    }
+    
+}
