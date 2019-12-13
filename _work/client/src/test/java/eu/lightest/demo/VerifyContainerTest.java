4d4df560e57dce0357e6c979104b8d0338aa007e
Stefan More <stefan.more@iaik.tugraz.at>
2019-12-13 21:07:41 +0100

    ec-demo-client: add support pades tests

--- _work/client/src/test/java/eu/lightest/demo/VerifyContainerTest.java
+++ _work/client/src/test/java/eu/lightest/demo/VerifyContainerTest.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -15,34 +16,43 @@ import static org.junit.Assert.assertTrue;
 @RunWith(Parameterized.class)
 public class VerifyContainerTest {
     
-    private static Object pathPolicy = "../policies/policy_pof_withTranslation.tpl";
+    private static final FilenameFilter filterAsic = (File dir, String name) -> name.endsWith(".asic") || name.endsWith(".asice") || name.endsWith(".asics");
+    private static final FilenameFilter filterPdf = (File dir, String name) -> name.endsWith(".pdf");
+    private static final Function<String, Boolean> allowedToFail = (String filename) -> filename.contains("failing");
     
+    private static Object pathPolicy = "../policies/policy_pof_withTranslation.tpl";
+    private static Object pathPolicyPades = "../policies/policy_pof_withTranslation_pades.tpl";
     @Parameterized.Parameter(0)
     public String container;
     
     @Parameterized.Parameter(1)
     public String policy;
     
-    @Parameterized.Parameters(name = "{0}")
+    @Parameterized.Parameter(2)
+    public String testName;
+    
+    @Parameterized.Parameters(name = "{2}")
     public static List<Object> data() {
         List<Object> data = new ArrayList<>();
         
-        final FilenameFilter filter = (File dir, String name) ->
-                name.endsWith(".asic") || name.endsWith(".asice") || name.endsWith(".asics");
         
         File container = new File(CreateContainer.orders);
-        for(File file : container.listFiles(filter)) {
-            data.add(new Object[]{file.getPath(), pathPolicy});
+        for(File file : container.listFiles(filterAsic)) {
+            data.add(new Object[]{file.getPath(), pathPolicy, "policy:XML   " + file.getName()});
+            data.add(new Object[]{file.getPath(), pathPolicyPades, "policy:PADES " + file.getName()});
+        }
+        for(File file : container.listFiles(filterPdf)) {
+            data.add(new Object[]{file.getPath(), pathPolicyPades, "policy:PADES " + file.getName()});
         }
         
         return data;
     }
     
     @Test
-    public void verifyContainer() {
+    public void verify() {
         boolean status = VerifyContainer.verify(container, policy);
         
-        if(this.container.contains("failing")) {
+        if(allowedToFail.apply(container)) {
             assertFalse(status);
         } else {
             assertTrue(status);
